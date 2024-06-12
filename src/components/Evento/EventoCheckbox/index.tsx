1d524a20c4035d69c5763f641cb6391a6b6e17ba
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-06-12 07:58:10 -0400

    Separando métodos de estados

---
+++ src/components/Evento/EventoCheckbox/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { IEvento } from '../../../interfaces/IEvento';
+import { useSetRecoilState } from 'recoil';
+import { listaDeEventosState } from '../../../state/atom';
+import useAtualizarEvento from '../../../hooks/useAtualizarEvento';
+
+const EventoCheckbox: React.FC<{ evento: IEvento }> = ({ evento }) => {
+  
+  const atualizarEvento = useAtualizarEvento();
+  const estilos = [
+    'far',
+    'fa-2x',
+    evento.completo ? 'fa-check-square' : 'fa-square'
+  ]
+  const alterarStatus = () => {
+    const eventoAtualizado = {
+      ...evento
+    }
+    eventoAtualizado.completo = !evento.completo
+
+    atualizarEvento(eventoAtualizado);
+  }
+
+  return (<i className={estilos.join(' ')} onClick={() => alterarStatus()}></i>)
+}
+
+export default EventoCheckbox
---
+++ src/hooks/useAtualizarEvento.ts
@@ -0,0 +1,15 @@
+import { useSetRecoilState } from "recoil"
+import { listaDeEventosState } from "../state/atom";
+import { IEvento } from "../interfaces/IEvento";
+
+const useAtualizarEvento = () => {
+  const setListaEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+  return (evento: IEvento) => {
+    return setListaEventos(listaAntiga => {
+      const indice = listaAntiga.findIndex(item => item.id === evento.id);
+      return [...listaAntiga.slice(0, indice), evento, ...listaAntiga.slice(indice + 1)];
+    });
+  }
+}
+
+export default useAtualizarEvento;
